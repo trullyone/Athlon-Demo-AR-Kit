2a66294cfbd2aa9b11f38b4996ff79610c8e35dd
Nickolay Kunev <trullyone@gmail.com>
2017-10-25 08:16:51 +0300

    Add Apple's AR framework & prepare project for customizations.

--- HelloARKit/ViewController.swift
+++ HelloARKit/ViewController.swift
@@ -7,19 +7,181 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    var spinner: UIActivityIndicatorView?
+    
+    let session = ARSession()
+    let standardConfiguration: ARWorldTrackingConfiguration = {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        return configuration
+    }()
+    
+    var virtualObjectManager: VirtualObjectManager!
+    
+    var screenCenter: CGPoint?
+    
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var addObjectButton: UIButton!
+    
+    let serialQueue = DispatchQueue(label: "com.divine.helloarkit.serialSceneKitQueue")
+    
+    var isLoadingObject: Bool = false {
+        didSet {
+            DispatchQueue.main.async {
+                //self.settingsButton.isEnabled = !self.isLoadingObject
+                self.addObjectButton.isEnabled = !self.isLoadingObject
+                //self.restartExperienceButton.isEnabled = !self.isLoadingObject
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupScene()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Prevent the screen from being dimmed after a while.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        if ARWorldTrackingConfiguration.isSupported {
+            // Start the ARSession.
+            resetTracking()
+        } else {
+            // This device does not support 6DOF world tracking.
+            let sessionErrorMsg = "This app requires world tracking. World tracking is only available on iOS devices with A9 processor or newer. " +
+            "Please quit the application."
+            print(sessionErrorMsg)
+            //displayErrorMessage(title: "Unsupported platform", message: sessionErrorMsg, allowRestart: false)
+        }
     }
-
-
+    
+    func setupScene() {
+        
+        // Synchronize updates via the `serialQueue`.
+        virtualObjectManager = VirtualObjectManager(updateQueue: serialQueue)
+        virtualObjectManager.delegate = self
+        
+        // set up scene view
+        sceneView.setup()
+        sceneView.delegate = self
+        sceneView.session = session
+        // sceneView.showsStatistics = true
+        
+        sceneView.scene.enableEnvironmentMapWithIntensity(25, queue: serialQueue)
+        
+        setupFocusSquare()
+        
+        DispatchQueue.main.async {
+            self.screenCenter = self.sceneView.bounds.mid
+        }
+    }
+    
+    func resetTracking() {
+        session.run(standardConfiguration, options: [.resetTracking, .removeExistingAnchors])
+        
+        /*textManager.scheduleMessage("FIND A SURFACE TO PLACE AN OBJECT",
+                                    inSeconds: 7.5,
+                                    messageType: .planeEstimation)*/
+    }
+    
+    // MARK: - Planes
+    
+    var planes = [ARPlaneAnchor: Plane]()
+    
+    func addPlane(node: SCNNode, anchor: ARPlaneAnchor) {
+        
+        let plane = Plane(anchor)
+        planes[anchor] = plane
+        node.addChildNode(plane)
+        
+        //textManager.cancelScheduledMessage(forType: .planeEstimation)
+        //textManager.showMessage("SURFACE DETECTED")
+        /*if virtualObjectManager.virtualObjects.isEmpty {
+            textManager.scheduleMessage("TAP + TO PLACE AN OBJECT", inSeconds: 7.5, messageType: .contentPlacement)
+        }*/
+    }
+    
+    func updatePlane(anchor: ARPlaneAnchor) {
+        if let plane = planes[anchor] {
+            plane.update(anchor)
+        }
+    }
+    
+    func removePlane(anchor: ARPlaneAnchor) {
+        if let plane = planes.removeValue(forKey: anchor) {
+            plane.removeFromParentNode()
+        }
+    }
+    
+    
+    // MARK: - Focus Square
+    
+    var focusSquare: FocusSquare?
+    
+    func setupFocusSquare() {
+        serialQueue.async {
+            self.focusSquare?.isHidden = true
+            self.focusSquare?.removeFromParentNode()
+            self.focusSquare = FocusSquare()
+            self.sceneView.scene.rootNode.addChildNode(self.focusSquare!)
+        }
+        
+        //textManager.scheduleMessage("TRY MOVING LEFT OR RIGHT", inSeconds: 5.0, messageType: .focusSquare)
+    }
+    
+    func updateFocusSquare() {
+        guard let screenCenter = screenCenter else { return }
+        
+        DispatchQueue.main.async {
+            var objectVisible = false
+            for object in self.virtualObjectManager.virtualObjects {
+                if self.sceneView.isNode(object, insideFrustumOf: self.sceneView.pointOfView!) {
+                    objectVisible = true
+                    break
+                }
+            }
+            
+            if objectVisible {
+                self.focusSquare?.hide()
+            } else {
+                self.focusSquare?.unhide()
+            }
+            
+            let (worldPos, planeAnchor, _) = self.virtualObjectManager.worldPositionFromScreenPosition(screenCenter,
+                                                                                                       in: self.sceneView,
+                                                                                                       objectPos: self.focusSquare?.simdPosition)
+            if let worldPos = worldPos {
+                self.serialQueue.async {
+                    self.focusSquare?.update(for: worldPos, planeAnchor: planeAnchor, camera: self.session.currentFrame?.camera)
+                }
+                //self.textManager.cancelScheduledMessage(forType: .focusSquare)
+            }
+        }
+    }
+    
+    @IBAction func addObjectTouchUpInside(_ sender: Any) {
+        if isLoadingObject { return }
+        
+        guard let cameraTransform = session.currentFrame?.camera.transform else {
+            return
+        }
+        
+        let definition = VirtualObjectDefinition(modelName: "cup", displayName: "cup")
+        let object = VirtualObject(definition: definition)
+        let position = focusSquare?.lastPosition ?? float3(0)
+        virtualObjectManager.loadVirtualObject(object, to: position, cameraTransform: cameraTransform)
+        if object.parent == nil {
+            serialQueue.async {
+                self.sceneView.scene.rootNode.addChildNode(object)
+            }
+        }
+    }
 }
 
